5513560937dcf380a4b69cbc0fecf9f91ddcd980
Bassam Helal <basshelal@gmail.com>
2020-07-24 00:09:25 +0100

    Something works! Frames extraction working!

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
+import {logD} from "./utils";
+
 console.log("Hello World!")
+
+const extractFrames = require('ffmpeg-extract-frames')
+
+async function main() {
+    // default behavior is to extract all frames
+    await extractFrames({
+        input: 'res/PIE.avi',
+        output: 'out/frame-%d.png'
+    })
+
+    logD("Finished extracting frames")
+    // generated screenshots:
+    // out/frame-1.png
+    // out/frame-2.png
+    // ...
+    // out/frame-100.png
+}
+
+main()
