5e00a3322730c24b802092691729e52f40655d4f
Bassam Helal <basshelal@gmail.com>
2020-08-09 22:58:36 +0100

    Changed Electron initialization window

--- src/Application.ts
+++ src/Application.ts
@@ -8,13 +8,10 @@ electron.app.whenReady().then(onReady)
 
 async function onReady() {
     browserWindow = new BrowserWindow({
-        center: true,
         width: 1280,
         height: 720,
         autoHideMenuBar: false,
-        darkTheme: true,
         webPreferences: {
-            javascript: true,
             nodeIntegration: true
         }
     })
