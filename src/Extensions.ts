442ccf4eaf5a8050a659f5da0bee491d6703815a
Bassam Helal <basshelal@gmail.com>
2020-08-21 23:09:52 +0100

    New way to add Extensions to objects and prototypes

--- src/Extensions.ts
+++ src/Extensions.ts
@@ -15,6 +15,8 @@ declare global {
 
         isEmpty(): boolean
 
+        isNotEmpty(): boolean
+
         pushAll(items: Iterable<T>): Array<T>
 
         flatten<R>(): Array<R>
@@ -52,124 +54,117 @@ declare global {
     }
 }
 
+function objExtension(object: any, extensionName: string, extension: Function) {
+    if (!object[extensionName]) object[extensionName] = extension
+}
+
+function protoExtension(object: any, extensionName: string, extension: Function) {
+    if (object.prototype) objExtension(object.prototype, extensionName, extension)
+}
+
 function _array() {
-    if (!Array.prototype.contains)
-        Array.prototype.contains = function <T>(this: Array<T>, element: T): boolean {
+    protoExtension(Array, "contains",
+        function <T>(this: Array<T>, element: T): boolean {
             return this.indexOf(element) >= 0
-        }
-
-    if (!Array.prototype.notContains)
-        Array.prototype.notContains = function <T>(this: Array<T>, element: T): boolean {
+        })
+    protoExtension(Array, "notContains",
+        function <T>(this: Array<T>, element: T): boolean {
             return !this.contains(element)
-        }
-
-    if (!Array.prototype.lastIndex)
-        Array.prototype.lastIndex = function (this: Array<any>): number {
+        })
+    protoExtension(Array, "lastIndex",
+        function (this: Array<any>): number {
             return this.length - 1
-        }
-
-    if (!Array.prototype.last)
-        Array.prototype.last = function <T>(this: Array<T>): T {
+        })
+    protoExtension(Array, "last",
+        function <T>(this: Array<T>): T {
             return this[this.length - 1]
-        }
-
-    if (!Array.prototype.remove)
-        Array.prototype.remove = function <T>(this: Array<T>, element: T) {
+        })
+    protoExtension(Array, "remove",
+        function <T>(this: Array<T>, element: T) {
             const index = this.indexOf(element)
             if (index >= 0) this.splice(index)
-        }
-
-    if (!Array.prototype.isEmpty)
-        Array.prototype.isEmpty = function (this: Array<any>) {
+        })
+    protoExtension(Array, "isEmpty",
+        function (this: Array<any>) {
             return this.length === 0
-        }
-
-    if (!Array.prototype.pushAll)
-        Array.prototype.pushAll = function <T>(this: Array<T>, items: Iterable<T>): Array<T> {
+        })
+    protoExtension(Array,
+        "isNotEmpty", function (this: Array<any>) {
+            return this.length !== 0
+        })
+    protoExtension(Array, "pushAll",
+        function <T>(this: Array<T>, items: Iterable<T>): Array<T> {
             for (let item of items) this.push(item)
             return this
-        }
-
-    if (!Array.init)
-        Array.init = function <T>(length: number, initializer: (index: number) => T): Array<T> {
+        })
+    objExtension(Array, "init",
+        function <T>(length: number, initializer: (index: number) => T): Array<T> {
             return Array.from({length: length}, (_, i) => initializer(i))
-        }
-
-    if (!Array.prototype.flatten)
-        Array.prototype.flatten = function <R>(this: Array<any>): Array<R> {
-            const result: Array<R> = []
-            this.forEach(it => {
-                if (it instanceof Array) it.flatten<R>().forEach(i => result.push(i))
-                else result.push(it)
-            })
-            return result
-        }
+        })
+    protoExtension(Array, "flatten", function <R>(this: Array<any>): Array<R> {
+        const result: Array<R> = []
+        this.forEach(it => {
+            if (it instanceof Array) it.flatten<R>().forEach(i => result.push(i))
+            else result.push(it)
+        })
+        return result
+    })
 }
 
 function _object() {
-
-    if (!Object.prototype.toJson)
-        Object.prototype.toJson = function (this: Object, space: number = 0): string {
+    protoExtension(Object, "toJson",
+        function (this: Object, space: number = 0): string {
             return JSON.stringify(this, null, space)
-        }
-
-    if (!Object.prototype.properties)
-        Object.prototype.properties = function (this: Object): Array<{ key: string, value: any, type: any }> {
+        })
+    protoExtension(Object, "properties",
+        function (this: Object): Array<{ key: string, value: any, type: any }> {
             if (this)
                 return Object.keys(this).map((key, index) => {
                     // @ts-ignore
                     return {key: key, value: this[key], type: typeof this[key]}
                 })
             else return []
-        }
-
-    if (!Object.prototype.also)
-        Object.prototype.also = function (this: Object, block: (it: Object) => void): Object {
+        })
+    protoExtension(Object, "also",
+        function (this: Object, block: (it: Object) => void): Object {
             if (this) block(this)
             return this
-        }
+        })
 }
 
 function _number() {
-    if (!Number.prototype.abs)
-        Number.prototype.abs = function (this: number): number {
+    protoExtension(Number, "abs",
+        function (this: number): number {
             return abs(this)
-        }
-
-    if (!Number.prototype.floor)
-        Number.prototype.floor = function (this: number): number {
+        })
+    protoExtension(Number, "floor",
+        function (this: number): number {
             return floor(this)
-        }
-
-    if (!Number.prototype.ceil)
-        Number.prototype.ceil = function (this: number): number {
+        })
+    protoExtension(Number, "ceil",
+        function (this: number): number {
             return ceil(this)
-        }
-
-    if (!Number.prototype.sqrt)
-        Number.prototype.sqrt = function (this: number): number {
+        })
+    protoExtension(Number, "sqrt",
+        function (this: number): number {
             return sqrt(this)
-        }
-
-    if (!Number.prototype.round)
-        Number.prototype.round = function (this: number): number {
+        })
+    protoExtension(Number, "round",
+        function (this: number): number {
             return round(this)
-        }
-
-    if (!Number.prototype.pow)
-        Number.prototype.pow = function (this: number, exponent: number): number {
+        })
+    protoExtension(Number, "pow",
+        function (this: number, exponent: number): number {
             return pow(this, exponent)
-        }
-
-    if (!Number.prototype.roundToDec)
-        Number.prototype.roundToDec = function (this: number, places: number = 0): number {
+        })
+    protoExtension(Number, "roundToDec",
+        function (this: number, places: number = 0): number {
             return +this.toFixed(places)
-        }
-
-    if (!Number.prototype.comma)
-        Number.prototype.comma = function (this: number): string {
+        })
+    protoExtension(Number, "comma",
+        function (this: number): string {
             return comma(this)
-        }
+        })
 }
 
 export default function () {
